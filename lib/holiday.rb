fae204fe5b528595a7802b611422e8174d45fd6d
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 02:40:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holiday|
-   season.each do |holiday, supplies|
+   holiday.each do |holiday, supplies|
      puts holiday, supplies
    end
  end
