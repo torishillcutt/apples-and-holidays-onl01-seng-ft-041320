9682d64309ab9c4b879f28ccd62b612359c83380
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 02:32:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-holiday_hash.keys.flatten
+holiday_hash.keys.values.flatten
 end
 
 def all_holidays_with_bbq(holiday_hash)
