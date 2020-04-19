d4f9f6f0ef10354e3225964b9bdee104b638406d
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 02:44:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,8 +54,8 @@ end
 def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holiday|
    holiday.each do |holiday, supplies|
-     binding.pry
      puts holiday, supplies
+     binding.pry
    end
  end
 end
