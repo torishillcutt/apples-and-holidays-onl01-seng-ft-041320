b188e10b3abd27035584a75a04ed37532f1fb34e
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 02:15:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,7 +36,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
         holiday << supply
       end
     end
-  end  
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
