92b4cb8b31508b574e6bfd8b91efcf2487d21602
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 03:33:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,11 +67,12 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   bbq_holidays = []
-holiday_hash.each do |season, holiday|
-  holiday.each do |holiday, supply|
-    if holiday.any?("BBQ")
-      bbq_holidays << holiday
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supply|
+      if holiday.any?("BBQ")
+        bbq_holidays << holiday
+      end
     end
-  end
+  end 
   bbq_holidays
 end
