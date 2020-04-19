4834cd613eb4df8d4531d48cec6b5873c9d3c9ea
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 02:31:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,18 +49,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
-
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+holiday_hash.keys.flatten
 end
 
 def all_holidays_with_bbq(holiday_hash)
