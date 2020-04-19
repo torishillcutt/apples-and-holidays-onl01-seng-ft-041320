ff0284731c89e789220ac0b6be14f18cb2f9856a
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 01:50:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-holiday_hash[:spring][:memorial_day].join
+holiday_hash[:summer][:fourth_of_july][2].join
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
