273bffdde47a13bf5536c07c8fd9d556f673e355
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 02:57:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
       puts supply
   end
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
