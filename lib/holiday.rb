ba3a99e24df7c54adcfd3581ea1d8ee4c78a028d
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 03:24:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday.each do |holiday| holiday.capitalize!
       holiday = holiday.join(" ")
 
-    puts "  #{holiday.capitalize}: #{supply.join(", ")}"
+    puts "  #{holiday}: #{supply.join(", ")}"
 
   end
 end
